688e535362eddf0ddd8f5c877652de21c4521263
maryja <marija.nevdach@gmail.com>
2018-09-15 12:27:52 +0300

    orders,filter

--- src/components/public/Orders.js
+++ src/components/public/Orders.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {orders, service} from '../../sandbox/named-exports';
 
-const Orders = (props)=>{
-  return (
-      <div className='orders'>
-        <h1>Orders page</h1>
-      </div>
-  );
-};
+
+class Orders extends React.Component{
+
+  state={
+    search:''
+  };
+  updateSearch(e){
+    this.setState({search:e.target.value.substr(0,20)});
+  }
+    render(){
+      let filteredOrders = orders.filter(
+          (order)=>{
+
+            return (
+                order.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1 ||
+                order.id.indexOf(this.state.search) !==-1
+          )
+          }
+      );
+        return (
+        <div>
+          <div className="search">
+            <input type="search"
+                   value={this.state.search}
+                   onChange={this.updateSearch.bind(this)}
+                   placeholder='paieška'/>
+          </div>
+          {filteredOrders.map((order)=>{
+            return(
+                <div>
+                  <ul>
+                    <li>
+                  <h2>{order.name}</h2>
+                  <p>{order.id}</p>
+                    </li>
+                  </ul>
+                </div>
+            )
+          })}
+        </div>
+        );
+    }
+}
 export default Orders
